a0f407915eba11ab6667aadbe3c005edb2521bda
Vikkuzz <vikkuzz@vk.com>
2024-10-09 17:14:41 +0300

    fix: test build

--- apps/www.royalfut/app/[locale]/page.tsx
+++ apps/www.royalfut/app/[locale]/page.tsx
@@ -117,9 +117,7 @@ const IndexPage = async () => {
                     </GradientButton>
                 </div>
             </LayoutViewportSectionFrame>
-            <LayoutViewportSectionFrame
-                dir={dir}
-                className="flex flex-col mt-24">
+            <LayoutViewportSectionFrame dir={dir} className="flex flex-col mt-24">
                 <SectionTitle>{t3("coins.options.title")}</SectionTitle>
                 <div className="flex gap-4 flex-col sm:flex-row">
                     {options.map(option => (
@@ -136,9 +134,7 @@ const IndexPage = async () => {
                 dir={dir}>
                 <FeatureHighlights cards={benefitCards} />
             </LayoutViewportSectionFrame>
-            <LayoutViewportSectionFrame
-                dir={dir}
-                className="flex flex-col mt-24">
+            <LayoutViewportSectionFrame dir={dir} className="flex flex-col mt-24">
                 <FAQSnippet.Title>{t4("faq.h1")}</FAQSnippet.Title>
                 <FAQSnippet.QA />
                 <FAQSnippet.CTA />
